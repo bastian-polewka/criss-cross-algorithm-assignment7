8c507ac91a7dce8602537a3d0c0df3cd70ae8e6a
creme332 <65414576+creme332@users.noreply.github.com>
2023-06-25 21:56:08 +0400

    add tests for constructor

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,5 +1,24 @@
+#include "../src/mul.h"
 #include "doctest.h"
 
-TEST_CASE("Equal") {
-  CHECK_EQ(1, 1);
+TEST_CASE("Constructor") {
+  SUBCASE("Empty strings") {
+    CHECK_THROWS_WITH(Mul("", ""), "Invalid string inputs");
+  }
+
+  SUBCASE("String with minus sign") {
+    CHECK_THROWS_WITH(Mul("-12443 ", "999"), "Invalid string inputs");
+  }
+
+  SUBCASE("String with non-numeric characters") {
+    CHECK_THROWS_WITH(Mul("two ", "32jhl&"), "Invalid string inputs");
+  }
+
+  SUBCASE("Numeric strings with spaces") {
+    CHECK_NOTHROW(Mul(" 1245 523 3443 ", "999 999 999"));
+  }
+
+  SUBCASE("Numeric strings with no spaces") {
+    CHECK_NOTHROW(Mul("1442", "99"));
+  }
 }
