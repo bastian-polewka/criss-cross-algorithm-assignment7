5b0a4b9a89727f6c441b738abbb20e9768f92f3a
creme332 <65414576+creme332@users.noreply.github.com>
2023-06-29 13:44:10 +0400

    update implementation
    clock is triggered when start is called.
    
    add a function to return duration

--- benchmarks/timer.cpp
+++ benchmarks/timer.cpp
@@ -1,6 +1,8 @@
-#include "timer.h";
+#include "timer.h"
 
-Timer::Timer() {
+Timer::Timer() {}
+
+void Timer::start() {
   startTime = std::chrono::high_resolution_clock::now();
 }
 
@@ -13,10 +15,13 @@ void Timer::stop() {
   auto end = std::chrono::time_point_cast<std::chrono::microseconds>(endTime)
                  .time_since_epoch()
                  .count();
-  auto duration = end - start;
-  double ms = duration * 0.001;
+  durationInMS = (end - start) * 0.001;
 }
 
 Timer::~Timer() {
   stop();
+}
+
+double Timer::duration() {
+  return durationInMS;
 }
