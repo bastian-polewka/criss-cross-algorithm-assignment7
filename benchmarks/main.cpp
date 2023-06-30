f734c3da796881f584e6b9c6a81d4d408f7c7e8a
creme332 <65414576+creme332@users.noreply.github.com>
2023-06-30 10:59:02 +0400

    add code to save to file and generate statistics

--- benchmarks/main.cpp
+++ benchmarks/main.cpp
@@ -1,4 +1,6 @@
+#include <algorithm>
 #include <cassert>
+#include <fstream>
 #include <iostream>
 #include <random>
 #include <string>
@@ -46,6 +48,74 @@ std::vector<std::vector<string>> getQuestions(ll n = 50, ll digitCount = 1000) {
   return result;
 }
 
+void saveQuestionsToFile(std::vector<std::vector<string>> questionArray) {
+  std::fstream file("output/input.txt", std::ios::out);
+  for (auto question : questionArray) {
+    file << question[0] << " " << question[1] << "\n";
+  }
+  file.close();
+}
+
+void saveProductToFile(std::vector<string> vResults,
+                       std::vector<string> kResults) {
+  assert(vResults.size() == kResults.size());
+  std::fstream file("output/product.txt", std::ios::out);
+  const int size = vResults.size();
+  for (int i = 0; i < size; i++) {
+    file << vResults[i] << " " << kResults[i] << "\n";
+  }
+
+  file.close();
+}
+
+double getMean(std::vector<double> arr) {
+  return (std::accumulate(arr.begin(), arr.end(), 0.0)) / arr.size();
+}
+
+double getVariance(std::vector<double> arr) {
+  double mean = getMean(arr);
+  double sum = 0;
+  for (auto e : arr) {
+    sum += (e - mean) * (e - mean);
+  }
+  return sum / arr.size();
+}
+
+void saveTimeToFile(std::vector<double> vDuration,
+                    std::vector<double> kDuration) {
+  assert(kDuration.size() == vDuration.size());
+  std::fstream file("output/time.txt", std::ios::out);
+  const int size = vDuration.size();
+
+  for (int i = 0; i < size; i++) {
+    file << vDuration[i] << " " << kDuration[i] << "\n";
+  }
+
+  file.close();
+}
+
+void outputStatistics(std::vector<double> vDuration,
+                      std::vector<double> kDuration) {
+  const double vedicMean = getMean(vDuration);
+  const double vedicVar = getVariance(vDuration);
+  std::cout << "-------------------------"
+            << "\n";
+  std::cout << "Mean time for Vedic = " << vedicMean << "\n";
+  std::cout << "Variance for Vedic = " << vedicVar << "\n";
+  std::cout << "-------------------------"
+            << "\n";
+
+  const double karaMean = getMean(kDuration);
+  const double karaVar = getVariance(kDuration);
+  std::cout << "Mean time for Karatsuba = " << karaMean << "\n";
+  std::cout << "Variance for Karatsuba = " << karaVar << "\n";
+  std::cout << "-------------------------"
+            << "\n";
+
+  std::cout << "Vedic is " << 100 * (vedicMean - karaMean) / karaMean
+            << " % faster.\n";
+}
+
 int main() {
   std::ios::sync_with_stdio(0);
   std::cin.tie(0);
@@ -55,12 +125,12 @@ int main() {
   // initialise parameters of benchmark
   const ll totalTestCases =
       100;  // number of multiplications to be perfomed for each algorithm
-  ll digitCount = 100;  // number of digits in multiplier and multiplicand
+  ll digitCount = 1000;  // number of digits in multiplier and multiplicand
 
   ll win = 0;
 
   // generate numbers to be multiplied
-  std::vector<std::vector<string>> quests =
+  std::vector<std::vector<string>> questionArray =
       getQuestions(totalTestCases, digitCount);
 
   // create arrays to store products calculated by each algorithm
@@ -75,7 +145,7 @@ int main() {
   Timer clock;
 
   string multiplicand, multiplier, product;
-  for (auto question : quests) {
+  for (auto question : questionArray) {
     // get numbers to be multiplied
     multiplicand = question[0];
     multiplier = question[1];
@@ -111,4 +181,8 @@ int main() {
   }
   std::cout << "Vedic won " << win << " times\n";
   std::cout << "Saving results to file...\n";
+  saveQuestionsToFile(questionArray);
+  saveProductToFile(vResults, kResults);
+  saveTimeToFile(vDuration, kDuration);
+  outputStatistics(vDuration, kDuration);
 }
